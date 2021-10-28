88083c630881e8353567f9b53474c11f87882067
Justin Fagan <jbfagan7@gmail.com>
2021-10-28 18:53:51 -0400

    Changed the typedef for a question

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,52 +1,51 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 module.exports = gql`
-
-    type User {
-        id: ID!
-        email: String!
-        token: String!
-        username: String!
-        createdAt: String!
-    }
-    type Question {
-      question: String!
-      answer: Int!
-      choices: [String!]!
-    }
-    type Quiz {
-      id: ID!
-      name: String!
-      description: String!
-      publishedDate: String
-      published: Boolean!
-      creator: String!
-      timesPlayed: Int!
-      time: String
-      rating: Int!
-      comments: [String!]!
-      difficulty: String!
-      color: String!
-      stats: String
-      content: [Question]!
-      backgroundImage: String
-      thumbnail: String
-      createdAt: String!
-    }
-    input RegisterInput {
-        username: String!
-        password: String!
-        confirmPassword: String!
-        email: String!
-    }
-    type Query {
-        getQuizzes: [Quiz]
-        getQuiz(quizId: ID!): Quiz!
-    }
-    type Mutation {
-        register(registerInput: RegisterInput): User!
-        login(username: String!, password: String!): User!
-        createQuiz( name: String!, creator: String! ): Quiz!
-        deleteQuiz(quizId: ID!): String!
-    }
-`
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+  type Question {
+    question: String!
+    answer: String!
+    choices: [String!]!
+  }
+  type Quiz {
+    id: ID!
+    name: String!
+    description: String!
+    publishedDate: String
+    published: Boolean!
+    creator: String!
+    timesPlayed: Int!
+    time: String
+    rating: Int!
+    comments: [String!]!
+    difficulty: String!
+    color: String!
+    stats: String
+    content: [Question]!
+    backgroundImage: String
+    thumbnail: String
+    createdAt: String!
+  }
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+  type Query {
+    getQuizzes: [Quiz]
+    getQuiz(quizId: ID!): Quiz!
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
+    createQuiz(name: String!, creator: String!): Quiz!
+    deleteQuiz(quizId: ID!): String!
+  }
+`;
