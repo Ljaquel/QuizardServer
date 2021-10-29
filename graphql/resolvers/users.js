0df2a585880fd519cbd9de9e00b517682d019b1e
Justin Fagan <jbfagan7@gmail.com>
2021-10-29 12:42:01 -0400

    Merging typedef changes

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,79 +1,103 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { UserInputError } = require('apollo-server');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
-const { validateRegisterInput, validateLoginInput } = require('../../util/validators')
-const { SECRET_KEY } = require('../../config');
-const User = require('../../models/User');
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
+const { SECRET_KEY } = require("../../config");
+const User = require("../../models/User");
+const checkAuth = require("../../util/check-auth");
 
 function generateToken(user) {
-    return jwt.sign(
-        {
-            _id: user._id,
-            email: user.email,
-            username: user.username
-        },
-        SECRET_KEY,
-        { expiresIn: '1h'}
-    );
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
 }
 
 module.exports = {
-  Query: {
-
-  },
+  Query: {},
   Mutation: {
     async login(_, { username, password }) {
-      console.log("Logging in...")
+      console.log("Logging in...");
       const { errors, valid } = validateLoginInput(username, password);
-      if(!valid) {
-        throw new UserInputError('Errors', { errors });
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
       }
       const user = await User.findOne({ username });
-      if(!user) {
+      if (!user) {
         errors.general = "User not found";
-        throw new UserInputError('User not found', { errors });
+        throw new UserInputError("User not found", { errors });
       }
       const match = await bcrypt.compare(password, user.password);
-      if(!match) {
+      if (!match) {
         errors.general = "Wrong credentials";
-        throw new UserInputError('Wrong credentials', { errors });
+        throw new UserInputError("Wrong credentials", { errors });
       }
       const token = generateToken(user);
-      return{
+      return {
         ...user._doc,
         _id: user._id,
-        token
-      }
+        token,
+      };
     },
-    async register( _, { registerInput: { username, email, password, confirmPassword } } ) {
-      console.log("Signing in...")
-      const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
-      if(!valid) {
-        throw new UserInputError('Errors', { errors });
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } }
+    ) {
+      console.log("Signing in...");
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
       }
       const user = await User.findOne({ username });
       if (user) {
-        throw new UserInputError('Username is taken', {
+        throw new UserInputError("Username is taken", {
           errors: {
-            username: 'This username is taken'
-          }
-        })
+            username: "This username is taken",
+          },
+        });
       }
       password = await bcrypt.hash(password, 12);
       const newUser = new User({
         email,
         username,
         password,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
       });
       const res = await newUser.save();
-      const token = generateToken(res)
-      return{
+      const token = generateToken(res);
+      return {
         ...res._doc,
         _id: res._id,
-        token
+        token,
+      };
+    },
+    changePassword: async (_, { newPassword, confirmPassword }, context) => {
+      const user = checkAuth(context);
+      try {
+        if (!user) return "You are not authenticated";
+        if (newPassword === confirmPassword) {
+          const password = await bcrypt.hash(newPassword, 10);
+          await User.findByIdAndUpdate(user._id, { password });
+          return "Password updated successfully";
+        }
+      } catch (err) {
+        return "There was an error updating your password";
       }
-    }
-  }
-}
+    },
+    updateUserFields: async (_, { updateFields: { email } }) => {},
+  },
+};
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,57 +1,60 @@
 const { gql } = require("apollo-server");
 
 module.exports = gql`
-    type User {
-        _id: ID!
-        email: String!
-        token: String!
-        username: String!
-        createdAt: String!
-    }
-    type Question {
-      question: String!
-      answer: Int!
-      choices: [String!]!
-    }
-    type Quiz {
-      _id: ID!
-      name: String!
-      description: String!
-      publishedDate: String
-      published: Boolean!
-      creator: String!
-      timesPlayed: Int!
-      time: String
-      rating: Int!
-      comments: [String!]!
-      difficulty: String!
-      color: String!
-      stats: String
-      content: [Question]!
-      backgroundImage: String
-      thumbnail: String
-      createdAt: String!
-    }
-    input RegisterInput {
-        username: String!
-        password: String!
-        confirmPassword: String!
-        email: String!
-    }
-    type Query {
-        getQuizzes: [Quiz]
-
-
-        getQuizzesbyUserid(creatorID: String!): [Quiz]
-      
-
-
-        getQuiz(quizId: ID!): Quiz!
-    }
-    type Mutation {
-        register(registerInput: RegisterInput): User!
-        login(username: String!, password: String!): User!
-        createQuiz( name: String!, creator: String! ): Quiz!
-        deleteQuiz(quizId: ID!): String!
-    }
+  type User {
+    _id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+  type Question {
+    question: String!
+    answer: Int!
+    choices: [String!]!
+  }
+  type Quiz {
+    _id: ID!
+    name: String!
+    description: String!
+    publishedDate: String
+    published: Boolean!
+    creator: ID!
+    timesPlayed: Int!
+    time: String
+    rating: Int!
+    comments: [String!]!
+    difficulty: String!
+    color: String!
+    stats: String
+    content: [Question]!
+    backgroundImage: String
+    thumbnail: String
+    createdAt: String!
+  }
+  input RegisterInput {
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+  input UserUpdateInput {
+    username: String!
+    firstName: String!
+    lastName: String!
+    email: String!
+  }
+  type Query {
+    getQuizzes: [Quiz]
+    getQuiz(quizId: ID!): Quiz!
+    getQuizzesbyUserid(creatorID: ID!): [Quiz]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
+    createQuiz(name: String!, creator: String!): Quiz!
+    deleteQuiz(quizId: ID!): String!
+    changePassword(newPassword: String!, confirmPassword: String!): String!
+    updateUserFields(updateFields: UserUpdateInput): User
+  }
 `;
